f0982c6702f66b935c066f35759cba3c1001926c
choubao <89794565+cyeo0023@users.noreply.github.com>
2022-10-15 21:46:08 +0800

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,5 +1,5 @@
-var spec2 = "week10Homework.json";
-var spec3 = "week9Homework.json";
+var spec2 = "bubble.json";
+var spec3 = "sphere.json";
 vegaEmbed('#bubble_plot', spec2).then(function(result) {
 }).catch(console.error);
 vegaEmbed('#map', spec3).then(function(result) {
